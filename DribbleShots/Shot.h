c671aba0328018d18a55c25e4763e15b8d69e470
Augustine Cheong <augustine.cheong@2359media.com>
2014-12-12 17:26:35 +0800

    Added basic data model

---
+++ DribbleShots/Shot.h
@@ -0,0 +1,19 @@
+//
+//  Shot.h
+//  DribbleShots
+//
+//  Created by Augustine on 12/12/14.
+//  Copyright (c) 2014 Augustine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Shot : NSManagedObject
+
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * imageURL;
+@property (nonatomic, retain) NSNumber * dribbleShotType;
+
+@end
---
+++ DribbleShots/Shot.m
@@ -0,0 +1,18 @@
+//
+//  Shot.m
+//  DribbleShots
+//
+//  Created by Augustine on 12/12/14.
+//  Copyright (c) 2014 Augustine. All rights reserved.
+//
+
+#import "Shot.h"
+
+
+@implementation Shot
+
+@dynamic title;
+@dynamic imageURL;
+@dynamic dribbleShotType;
+
+@end
